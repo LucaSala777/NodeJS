0f5cb02f3c5a3a22f4958b0acb5eacdce5b2826f
Luca Sala <73589421+LucaSala777@users.noreply.github.com>
2021-03-08 16:59:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 var http = require("http");
-var hostname="localhost";
-var port= 3000;
-var nome="NODESALA"
-var express=require("express");
-const { readSync } = require("fs");
+
+var express = require("express");
 var apiServer = express();
 
-var server= http.createServer((req, res) =>{
-    req.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Bellaaaaa");
+var port = 3000;
+
+apiServer.listen(port, () => {
+    console.log("server running on port ", port);
 });
 
-server.listen(port, hostname, () => {
-    console.log("server running at http://%s:%d/", hostname, port);
-});
-
-apiServer.get("/nome", ()=>{
-console.log("ho ricevuto una get su nome");
-res.statusCode=200;
-res.setHeader("content-Type", "text/plain");
-    res.send("Ciao, il nome del server è ",nome);
+apiServer.get("/nome", (req, res) =>{
+    console.log("ho ricevuto una get su nome");
+    res.send("ciao, il nome del server è: NODEPOGLIANI");
 });
 
 apiServer.get("/", function(req, res){
-    res.statusCode=200;
-res.setHeader("content-Type", "text/plain");
     res.send("sei in home");
-});
-
-apiServer.get("/index", function(req, res){
-res.sendFile("C:\\Users\\sala.lucaandrea\\Desktop\\Nuova cartella\\NodeJS\\index.html");
-});
+})
